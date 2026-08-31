20f7e385ff4d0af0a1064a3674e5c197e0a0b996
movsb <chkesp@gmail.com>
2026-08-31 14:23:23 +0800

    stash

---
+++ demo/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"embed"
+	_ "embed"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+
+	"github.com/movsb/fbiw"
+)
+
+// pprof 性能测试用。
+//
+// go tool pprof -web  http://localhost:8888/debug/pprof/profile?seconds=30
+func init() {
+	go http.ListenAndServe(`0.0.0.0:8888`, nil)
+}
+
+//go:embed *.html
+var embedded embed.FS
+
+func main() {
+	app := fbiw.NewApp()
+	defer app.Close()
+
+	app.AddFont(`system`, false, false, os.DirFS(`.`), `regular.ttf`)
+
+	app.NewDesktop(embedded, `main.html`)
+
+	app.Run()
+}
